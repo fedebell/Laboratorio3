920c21f2fdda55a37b57a72962a1aefac1299105
mcosta18 <marcocosta.95.pi@hotmail.it>
2017-05-15 15:56:24 +0200

    corretti grafici; modificate alcune cosette. Soprattutto bisogna riguardare bene se mnancano immagini e aggiustare le ref

--- relazione14/plot.py
+++ relazione14/plot.py
@@ -4,9 +4,9 @@ from scipy.optimize import curve_fit
 import matplotlib
 import math
 
-n, V, dV = pylab.loadtxt('C:\\Users\\Lisa\\Desktop\\Lab3\\Lock-In\\dati2marco.txt', unpack=True)
+n, V, dV = pylab.loadtxt('C:\\Users\\marco\\Desktop\\Laboratorio3\\relazione14\\dati1(lisa).txt', unpack=True)
 
-V= -1*V
+V= 1*V
 y=pylab.log(V)
 dy=dV/V
 
@@ -14,16 +14,16 @@ pylab.figure(1)
 
 pylab.rc('font',size=13)
 
-pylab.title('Assorbimento', fontsize = "16")
+pylab.title('Assorbimento (no Lock-In)', fontsize = "16")
 
 pylab.xlabel('numero lastrine', size = "14")
 
 pylab.ylabel('$V_{out}$ [$V$]', size = "14")
-
+#plotto direttamente le cose che stanno in esponenziale, dopo tanto metto y=logscale
 pylab.xlim(-0.5,7.5)
 pylab.grid(color = "gray")
-pylab.plot(n,y, '.', label='data', color = 'black')
-pylab.errorbar(n, y, dy, linestyle='', marker='+', color = 'black')
+#pylab.plot(n,y, '.', label='data', color = 'black')
+pylab.errorbar(n, V, dV, linestyle='', marker='+', color = 'blue')
 
 def linear(x,a,b):
     return a*x+b
@@ -52,22 +52,23 @@ ndof = len(n) - 2 #Tolgo due parametri estratti dal fit
 print("Chisquare/ndof = %f/%d" % (somma, ndof))
 #print("p = ", p)
 
-#Routine per stampare due rette:
+#Routine per stampare due rette; ho smanacciato sui limiti del bucket per fare venire una bella retta
 div = 1000
 bucket = numpy.array([0.0 for i in range(div)])
 retta = numpy.array([0.0 for i in range(div)])
-inc = (n.max()-n.min())/div 
+inc = (n.max()+3-n.min()-1)/div 
 for i in range(len(bucket)):
-        bucket[i]=float(i)*inc + n.min()
+        bucket[i]=float(i)*inc + n.min()-1
         retta[i] = linear(bucket[i], par[0], par[1])
-pylab.plot(bucket, retta, color = "black")
+#quello che sto per fare è macchinoso ma funziona: esponenzio quello che ho e poi metto y in logaritmica
+pylab.plot(bucket, pylab.exp(retta), color = "black")
+pylab.yscale('log')
 
-
+pylab.ylim(0.7,10)
 da=cov[0][0]**0.5
 db=cov[1][1]**0.5
 
 print("Si ricavano a = ", a ,"+-" ,da, "e di b =",  b, "+-", db)
-
-
+print("Ossia n0 = ", -1/a, "pm", da/a**2)
 
 pylab.show()
